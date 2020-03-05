12def55f1a6b76265cc9892690c7ed25638e9cb3
juliansan14 <juliansan14@hotmail.es>
2020-03-04 19:10:53 -0500

    pago facil

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//ruta de arreglos
+//multidimensiones
+Route::get('arreglosm',function(){ 
+ $paises=[ "colombia"=> ["capital"=> "bogota",
+                        "peso"=>"peso",
+                        "poblacion"=>50],
+
+            "peru"=> ["capital"=> "lima",
+                        "peso"=>"sol",
+                        "poblacion"=>32],
+
+            "bolivia"=> ["capital"=> "la paz",
+                        "peso"=>"bolivariano",
+                         "poblacion"=>11] ];
+            echo"<pre>";
+    var_dump($paises);
+    echo"</pre>";
+
+    //recorrrer el arreglo de paises
+    foreach($paises as $pais => $informacion){
+        echo"<h1>$pais:</h1>";
+        foreach($informacion as $clave=> $valor){
+            echo "$clave: $valor"."<br/>";
+        }
+        //echo $informacion["capital"]."<br/>";
+        echo"<hr/>";
+    }
+
+});
